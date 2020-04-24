550f2364fad8cca6a6dabb439f1dbb124e38a71c
bxm <1759567121@qq.com>
2020-04-24 16:39:55 +0800

    add

--- re-cvae-vad-rank/cvae.py
+++ re-cvae-vad-rank/cvae.py
@@ -14,9 +14,9 @@ import numpy as np
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--trainset_path', dest='trainset_path', default='data/raw/dialogues_train_singleturn.txt', type=str, help='训练集位置')
-parser.add_argument('--validset_path', dest='validset_path', default='data/raw/dialogues_validation_singleturn.txt', type=str, help='验证集位置')
-parser.add_argument('--testset_path', dest='testset_path', default='data/raw/dialogues_test_singleturn.txt', type=str, help='测试集位置')
+parser.add_argument('--trainset_path', dest='trainset_path', default='data/raw/newtrainset.txt', type=str, help='训练集位置')
+parser.add_argument('--validset_path', dest='validset_path', default='data/raw/newvalidset.txt', type=str, help='验证集位置')
+parser.add_argument('--testset_path', dest='testset_path', default='data/raw/newtestset.txt', type=str, help='测试集位置')
 parser.add_argument('--embed_path', dest='embed_path', default='data/embed.txt', type=str, help='vad位置')
 parser.add_argument('--vad_path', dest='vad_path', default='data/vad.txt', type=str, help='vad位置')
 parser.add_argument('--result_path', dest='result_path', default='result', type=str, help='测试结果位置')
@@ -29,7 +29,7 @@ parser.add_argument('--sample_times', dest='sample_times', default=20, type=int,
 parser.add_argument('--model_path', dest='model_path', default='log/040000000190160.model', type=str, help='载入模型位置')  #
 parser.add_argument('--seed', dest='seed', default=666, type=int, help='随机种子')  #
 parser.add_argument('--gpu', dest='gpu', default=True, type=bool, help='是否使用gpu')  #
-parser.add_argument('--max_epoch', dest='max_epoch', default=40, type=int, help='最大训练epoch')
+parser.add_argument('--max_epoch', dest='max_epoch', default=16, type=int, help='最大训练epoch')
 
 args = parser.parse_args()  # 程序运行参数
 
--- re-cvae-vad-rank/model/util/config.py
+++ re-cvae-vad-rank/model/util/config.py
@@ -11,31 +11,31 @@ class Config(object):
     embedding_size = 300
 
     # 编码器参数
-    encoder_cell_type = 'GRU'  # in ['GRU', 'LSTM']
-    encoder_output_size = 128
-    encoder_num_layers = 1
+    encoder_cell_type = 'LSTM'  # in ['GRU', 'LSTM']
+    encoder_output_size = 300
+    encoder_num_layers = 2
     encoder_bidirectional = True  # 是否是双向rnn
 
     # 解码器参数
-    decoder_cell_type = 'GRU'  # in ['GRU', 'LSTM']
-    decoder_output_size = 128
-    decoder_num_layers = 1
+    decoder_cell_type = 'LSTM'  # in ['GRU', 'LSTM']
+    decoder_output_size = 300
+    decoder_num_layers = 2
 
     # 潜变量参数
-    latent_size = 100
+    latent_size = 200
 
     # 先验网络参数
-    dims_prior = [110]
-    dims_recognize = [110]
+    dims_prior = [250]
+    dims_recognize = [250]
 
-    batch_size = 16
+    batch_size = 32
     method = 'Adam'  # in ['SGD', 'Adam', 'Adamax', 'RMSprop']
     lr = 0.0001  # 初始学习率
     lr_decay = 1.0  # 学习率衰减，每过1个epoch衰减的百分比
     weight_decay = 0  # 权值decay
     eps = 1e-8
     max_grad_norm = 5
-    kl_step = 4000  # 更新多少次参数之后kl项权值达到1
+    kl_step = 10000  # 更新多少次参数之后kl项权值达到1
     dropout = 0.0
 
 
